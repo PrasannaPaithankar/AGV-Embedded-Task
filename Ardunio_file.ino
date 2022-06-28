430b3e22fc1bfc031dc5c8923d56bbca0246a7e9
Prasanna Paithankar <82210129+PrasannaPaithankar@users.noreply.github.com>
2022-06-28 23:17:56 +0530

    Update Ardunio_file.ino

--- Ardunio_file.ino
+++ Ardunio_file.ino
@@ -9,15 +9,15 @@ float x_pred[3][1], P_pred[3][3], L[3][3], L_cpy[3][3], P_predC_trans[3][1];
 float temp1[3][3], temp3[3][1], temp4[1][1];
 
 float  SoC = 1;
-float  V_ct = 0;
-float  V_df = 0;
-float  tau_ct = 1e-5;
-float  tau_df = 1e-5;
+float  V_ct = 0.007;
+float  V_df = 0.004;
+float  tau_ct = 4;
+float  tau_df = 100;
 float  R_ct = 0.001;
-float  R_df = 0.001;
+float  R_df = 0.01;
 float  R_0 = 0.2;
 float  R_l = 10;
-float  Charge = 0.6;
+float  Charge = 0.6*3600;
   
 float I = 0;
 
@@ -26,12 +26,12 @@ float x[3][1] = {{SoC},
                  {V_df}};
                 
 float A[3][3] = {{1, 0, 0},
-                 {0, exp(-del_t/tau_ct), 0},
-                 {0, 0, exp(-del_t/tau_df)}};
+                 {0, exp(-(del_t/1000)/tau_ct), 0},
+                 {0, 0, exp(-(del_t/1000)/tau_df)}};
                 
-float B[3][1] = {{-del_t/Charge},
-                 {R_ct*(1-exp(-del_t/tau_ct))},
-                 {R_df*(1-exp(-del_t/tau_df))}};
+float B[3][1] = {{-(del_t/1000)/Charge},
+                 {R_ct*(1-exp(-(del_t/1000)/tau_ct))},
+                 {R_df*(1-exp(-(del_t/1000)/tau_df))}};
 
 float P[3][3] = {{1e-3, 0, 0},
                  {0, 1e-6, 0},
@@ -39,13 +39,13 @@ float P[3][3] = {{1e-3, 0, 0},
 
 float R[1] = {1e-3};
 
-float Q[3][3] = {{1, 0, 0},
+float Q[3][3] = {{0, 0, 0},
                  {0, 1e-4, 0},
-                 {0, 0, 1e-4}};
+                 {0, 0, 1e-5}};
 
 float C[1][3] = {1, -1, -1};
 
-float C_trans[3][1] = {{ 1},
+float C_trans[3][1] = {{ 0},
                        {-1},
                        {-1}};
 
@@ -76,7 +76,7 @@ void loop() {
   
   //Extended Kalman Estimator
 
-  /* Algorithms based on
+  /* Algorithms based on 
    * Carlo Taborelli, Simona Onori. "State of Charge Estimation
    * Using Extended Kalman Filters for Battery Management System".
    */
@@ -91,6 +91,14 @@ void loop() {
   Matrix.Multiply((mtx_type*)temp1, (mtx_type*)A, 3, 3, 3, (mtx_type*)temp1);
   Matrix.Add((mtx_type*)temp1, (mtx_type*)Q, 3, 1, (mtx_type*)P_pred); 
 
+  /*Parameter update
+   * Values based on 
+   * yu, Zhihao & Huai, Ruituo & Xiao, Linjing. (2015). 
+   * "State-of-Charge Estimation for Lithium-Ion Batteries 
+   * Using a Kalman Filter Based on Local Linearization".
+   */
+  C[0][0] = (3149.3 - (288.24/x_pred[0][0]) - (0.5748*x_pred[0][0]) + (54.8063*log(x_pred[0][0]))- (29.833*log(1-x_pred[0][0])))/(2*x_pred[0][0]);
+  
   //20a 
   Matrix.Multiply((mtx_type*)P_pred, (mtx_type*)C_trans, 3, 3, 1, (mtx_type*)P_predC_trans);
   Matrix.Multiply((mtx_type*)C, (mtx_type*)P_predC_trans, 1, 3, 1, (mtx_type*)temp4);
@@ -115,5 +123,8 @@ void loop() {
   
   //Output
   Serial.println(x[0][0]);
+  Serial.println(I);
   Serial.println(V_ocv);
+
+
 }
